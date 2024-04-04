01301644fa5a478928e0dc4d98f188229285620b
Jack Turk <jackturk5@outlook.com>
2024-04-04 17:18:41 -0500

    Added employee model

---
+++ src/main/java/com/onward/app/entities/Customer.java
@@ -0,0 +1,22 @@
+package com.onward.app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+}
---
+++ src/main/java/com/onward/app/services/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.onward.app.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.onward.app.entities.Employee;
+import com.onward.app.repositories.EmployeeRepositroy;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepositroy employeeRepositroy;
+
+    public Employee saveEmployee(Employee emp){
+        return employeeRepositroy.save(emp);
+    }
+
+    public List<Employee> fetchEmployees(){
+        return (List<Employee>) employeeRepositroy.findAll();
+    }
+
+    public Employee updateEmployee(Employee emp){
+        Employee db = employeeRepositroy.findById(emp.getId()).get();
+        
+        if(emp.getName() != db.getName()){
+            emp.setName(db.getName());
+        }
+
+        if(emp.getRole() != db.getRole()){
+            emp.setRole(db.getRole());
+        }
+
+        return employeeRepositroy.save(emp);
+    }
+
+    public void deleteEmployeeById(Long id){
+        employeeRepositroy.deleteById(id);
+    }
+
+}
